9c8102ebdb6fe8512b22d02204daf993c47565c6
Larina <Losiknor@gmail.com>
2022-02-21 02:16:18 +0700

    Решение задачи 42 вторым способом

--- zadacha_42/Program.cs
+++ zadacha_42/Program.cs
@@ -1,11 +1,30 @@
 ﻿// Задача 42: Определить, сколько чисел больше 0 введено с клавиатуры.
+//
+//          СПОСОБ 1
+//
+// Console.WriteLine("Введите числа, разделённые пробелом");
+// string str = Console.ReadLine();
+// string[] split = str.Split(new Char[] { ' ', ',', '.', ':' });
+// int count = 0;
+// foreach (string s in split)
+// {
+//     if (Convert.ToInt32(s) > 0)
+//        {count = count + 1;}
+// }
+// Console.WriteLine(count);
+//
+//
+//          СПОСОБ 2
+//
 Console.WriteLine("Введите числа, разделённые пробелом");
 string str = Console.ReadLine();
-string[] split = str.Split(new Char[] { ' ', ',', '.', ':' });
+string[] split = str.Split(new Char[] { ' '});
 int count = 0;
 foreach (string s in split)
 {
-    if (Convert.ToInt32(s) > 0)
-       {count = count + 1;}
+    int number;
+    bool success = int.TryParse(s, out number);
+if (success && (Convert.ToInt32(s) > 0)) 
+count = count + 1;
 }
 Console.WriteLine(count);
